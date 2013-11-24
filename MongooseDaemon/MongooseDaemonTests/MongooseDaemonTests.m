a2c06e7748deadcd8bb407a78a69130e682d2f9b
José Ibañez <jose_ibanez@cable.comcast.com>
2013-11-24 14:42:50 -0500

    Universal iOS static lib/Mac OS framework

---
+++ MongooseDaemon/MongooseDaemonTests/MongooseDaemonTests.m
@@ -0,0 +1,34 @@
+//
+//  MongooseDaemonTests.m
+//  MongooseDaemonTests
+//
+//  Created by Ibanez, Jose on 11/24/13.
+//  Copyright (c) 2013 CIM. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MongooseDaemonTests : XCTestCase
+
+@end
+
+@implementation MongooseDaemonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
